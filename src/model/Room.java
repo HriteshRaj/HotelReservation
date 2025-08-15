f9bff4f559a7bcf499aca9cb4336c0a98d0627bb
Hritesh <hriteshraj102@gmail.com>
2025-08-15 12:25:01 +0530

    Room Class Created

--- src/model/Room.java
+++ src/model/Room.java
@@ -1,4 +1,36 @@
 package model;
 
-public class Room {
+public class Room implements  IRoom{
+
+    String roomNumber;
+    Double price;
+    RoomType roomType;
+
+
+    @Override
+    public String getRoomNumber() {
+        return "";
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return 0.0;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return null;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+       return "room number =" + roomNumber +
+               "room price = "+ price
+               +" room type = " + roomType;
+    }
 }
