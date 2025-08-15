9143f2a113d4c57aec86e9c75cf4bddfc0654432
Hritesh <hriteshraj102@gmail.com>
2025-08-15 17:19:22 +0530

    Reservation functionalities added

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -10,6 +10,46 @@ public class Reservation {
     IRoom room;
     Date checkIntDate;
     Date checkOutDate;
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public IRoom getRoom() {
+        return room;
+    }
+
+    public void setRoom(IRoom room) {
+        this.room = room;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Date getCheckIntDate() {
+        return checkIntDate;
+    }
+
+    public void setCheckIntDate(Date checkIntDate) {
+        this.checkIntDate = checkIntDate;
+    }
+
+
+
+    public Reservation(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
+        this.customer=customer;
+        this.room=room;
+        this.checkIntDate=checkInDate;
+        this.checkOutDate=checkOutDate;
+    }
 
     @Override
     public String toString() {
---
+++ src/service/ReservationService.java
@@ -0,0 +1,73 @@
+package service;
+
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+
+import java.util.*;
+
+public class ReservationService {
+
+    ArrayList<Reservation> storeReservation = new ArrayList<>();
+    HashMap<String,IRoom> rooms = new HashMap<>();
+
+
+    public void addRoom(IRoom room){
+    rooms.put(room.getRoomNumber(),room);
+    }
+
+    public IRoom getARoom(String roomId){
+       return rooms.get(roomId);
+    }
+
+    public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+    Reservation res = new Reservation(customer,room,checkInDate,checkOutDate);
+    storeReservation.add(res);
+    return  res;
+
+    }
+    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate){
+    Collection<IRoom> availableRooms = new ArrayList<>();
+    for(IRoom room:rooms.values()){
+        int flag=1;
+        for(Reservation res: storeReservation){
+            if(res.getRoom().getRoomNumber().equals(room.getRoomNumber())){
+                flag=0;
+                break;
+            }
+
+        }
+        if(flag==1){
+            availableRooms.add(room);
+        }
+    }
+    return  availableRooms;
+
+
+
+
+    }
+    public Collection<Reservation> getCustomersReservation(Customer customer){
+    Collection<Reservation> custReservations = new ArrayList<>();
+    for(Reservation res: storeReservation){
+        if(res.getCustomer().equals(customer)){
+            custReservations.add(res);
+        }
+    }
+    return  custReservations;
+    }
+    public void printAllReservation(){
+    if(storeReservation.size()==0){
+        System.out.println("could not find any results");
+    }
+    for(Reservation res:storeReservation){
+        System.out.println(res);
+    }
+
+    }
+
+
+
+
+
+}
