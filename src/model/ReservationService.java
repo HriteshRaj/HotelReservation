238cb67725ffa48af6954949a1ff58d87bd6215c
Hritesh <hriteshraj102@gmail.com>
2025-08-15 15:43:40 +0530

    Reservation Service Added

---
+++ src/model/ReservationService.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Stack;
+
+public class ReservationService {
+
+    Collection<IRoom> storeReservation = new Stack<>();
+
+    public void addRoom(IRoom room){
+
+    }
+
+    public IRoom getARoom(String roomId){
+
+    }
+
+    public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
+
+    }
+    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate){
+
+    }
+    public Collection<Reservation> getCustomersReservation(Customer customer){
+
+    }
+    public void printAllReservation(){
+
+    }
+
+
+
+
+
+}
