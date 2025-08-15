142c683d9063b248bd1fc5bdb11f99b29ac2f3ca
Hritesh <hriteshraj102@gmail.com>
2025-08-15 16:33:25 +0530

    Customer data stored in hashmap

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -2,20 +2,27 @@ package service;
 
 import model.Customer;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 
 public class CustomerService {
+
+    static HashMap<String,Customer> customers = new HashMap<>();
+
 
 
     public void addCustomer(String email,String firstName,String lastName){
         Customer customer = new Customer(email,firstName,lastName);
+        customers.put(email,customer);
     }
 
     public static Customer getCustomer(String customerEmail){
         return null;
     }
-    public static Collection<Customer> getAllCustomers(){
-        return null;
+    public  Collection<Customer> getAllCustomers(){
+        Collection<Customer> customerLists =customers.values();
+        return  customerLists;
     }
 
 
